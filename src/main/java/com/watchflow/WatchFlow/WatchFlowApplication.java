4205952d8f6471f79c6d1c0cadd05380e60ba38a
Nicolas Cleik <nicolas@cleik.dev>
2026-04-22 11:22:03 -0300

    Initial commit

---
+++ src/main/java/com/watchflow/WatchFlow/WatchFlowApplication.java
@@ -0,0 +1,13 @@
+package com.watchflow.WatchFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watchflow/WatchFlow/WatchFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchflow.WatchFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7384d20d9a1e40ab3b1176a8d4339d967324ae63
bru <Brunnopassos1@gmail.com>
2026-06-03 00:30:37 -0300

    Update WatchFlowApplication.java

--- src/main/java/com/watchflow/WatchFlow/WatchFlowApplication.java
+++ src/main/java/com/watchflow/WatchFlow/WatchFlowApplication.java
@@ -2,12 +2,13 @@ package com.watchflow.WatchFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WatchFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchFlowApplication.class, args);
 	}
-
 }
